03ae470a32b09ea1d335186c5042fcf81175e763
ogneslav3000 <89992293214a@gmail.com>
2023-03-10 13:39:35 +0300

    Creating bottomNavigationView, change color, icons, text

---
+++ app/src/main/java/com/example/buttonnavigationview/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.buttonnavigationview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.buttonnavigationview.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.bNov.selectedItemId = R.id.item4
+        binding.bNov.setOnNavigationItemSelectedListener {
+            when(it.itemId) {
+                R.id.item1 -> {
+                    Toast.makeText(this, "Item 1", Toast.LENGTH_SHORT).show()
+                }
+                R.id.item2 -> {
+                    Toast.makeText(this, "Item 2", Toast.LENGTH_SHORT).show()
+                }
+                R.id.item3 -> {
+                    Toast.makeText(this, "Item 3", Toast.LENGTH_SHORT).show()
+                }
+                R.id.item4 -> {
+                    Toast.makeText(this, "Item 4", Toast.LENGTH_SHORT).show()
+                }
+            }
+            true
+        }
+    }
+}
